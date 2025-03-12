d69d81a1f60f21336ff60ed394ea979d34dd59e7
aswin-w <aswinwiselin24@gmail.com>
2025-03-12 14:17:50 +0530

    commands added

---
+++ commands/tom.java
@@ -0,0 +1,10 @@
+public class tom{
+    public static void main(String  arg[]){
+        int a=10;//this is int value
+        System.out.println(a);
+        /*
+        1)demo file.
+        2)using int variable.
+        */
+}
+}
