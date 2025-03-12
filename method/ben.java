d30e062bdf5c37c97821546a21e0e2d657eedd76
aswin-w <aswinwiselin24@gmail.com>
2025-03-12 14:34:30 +0530

    method is used

---
+++ method/ben.java
@@ -0,0 +1,12 @@
+public class ben{
+    void ben(){
+        int a=10;
+        int b=40;
+        int c=a+b;
+        System.out.println("addition is "+c);
+}
+public static void main(String[] args){
+    ben obj=new ben();
+    obj.ben();
+}
+}
