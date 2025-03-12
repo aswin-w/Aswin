f95a2afdfc1880d67e3ae7f4873f7b5d0d947260
aswin-w <aswinwiselin24@gmail.com>
2025-03-12 14:58:25 +0530

    returntype is used

---
+++ returntype/luffy.java
@@ -0,0 +1,11 @@
+public class luffy{
+    public int add(int a,int b){
+        int c=a+b;
+        return c;
+    }
+    public static void main(String arg[]){
+        luffy obj=new luffy();
+        int d=obj.add(10,20);
+         System.out.println(d);
+    }
+}
