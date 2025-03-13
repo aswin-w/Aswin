eb8a35ae30468b0713878a6118265425a5e5fb81
aswin-w <aswinwiselin24@gmail.com>
2025-03-13 11:38:38 +0530

    finished

---
+++ positivenegative/pos.java
@@ -0,0 +1,12 @@
+public class pos{
+    public static void main(String[] args){
+       int a=10;
+        if (a>0){
+            System.out.println("this number is positive.");
+        }
+        else{
+            System.out.println("this number is negative.");
+        }
+    }
+
+}
