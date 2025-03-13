30c600e65631b92cf620cb40374cc0c75a646dd1
aswin-w <aswinwiselin24@gmail.com>
2025-03-13 14:24:32 +0530

    odd finished

---
+++ problem3/demo.java
@@ -0,0 +1,9 @@
+public class demo{
+    public static void main(String[] args){
+        for(int i=27;i<=38;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
