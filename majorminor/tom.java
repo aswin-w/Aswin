aeba25d43f8f3a4a426ed51ca06ca7418de91db6
aswin-w <aswinwiselin24@gmail.com>
2025-03-13 11:34:04 +0530

    majorminor finish

---
+++ majorminor/tom.java
@@ -0,0 +1,12 @@
+public class tom{
+    public static void main(String[] args){
+int a=19;
+if(a>18){
+            System.out.println("this is major");
+        }
+        else{
+            System.out.println("this is minor");
+        }
+    }
+
+}
