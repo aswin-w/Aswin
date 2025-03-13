bbdf892fa7685de1796fe74dc65a7497c1764781
aswin-w <aswinwiselin24@gmail.com>
2025-03-13 11:09:30 +0530

    ifelse finish

---
+++ ifelseconiditional/ifelse.java
@@ -0,0 +1,12 @@
+public class ifelse{
+    public static void main(String[] args){
+        boolean a=false;
+        if (a==true){
+            System.out.println("the a value is true.");
+        }
+        else{
+            System.out.println("the value is false.");
+        }
+    }
+
+}
