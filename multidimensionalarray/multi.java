8f4ec939514755b83c0e1d59874d59ec7c7d90c9
aswin-w <aswinwiselin24@gmail.com>
2025-03-13 13:43:46 +0530

    multidimensional finish

---
+++ multidimensionalarray/multi.java
@@ -0,0 +1,11 @@
+public class multi{
+    public static void main(String[] args){
+        int a[][]={{10,20},{12,23}};
+        System.out.println(a[0][0]);
+        System.out.println(a[0][1]);
+        System.out.println(a[1][0]);
+        System.out.println(a[1][1]);
+
+
+    }
+}
