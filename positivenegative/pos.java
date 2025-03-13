751354b6a58d633e5e50af1a77961861ae3570c9
aswin-w <aswinwiselin24@gmail.com>
2025-03-13 11:49:30 +0530

    positive or negative

--- positivenegative/pos.java
+++ positivenegative/pos.java
@@ -1,8 +1,11 @@
 public class pos{
     public static void main(String[] args){
-       int a=10;
+       int a=-10;
         if (a>0){
             System.out.println("this number is positive.");
+        }
+        else if(a==0){
+            System.out.println("this number is nuteral."); 
         }
         else{
             System.out.println("this number is negative.");
