62fb1227fd7ddf724385f60f442519067cee2b00
aswin-w <aswinwiselin24@gmail.com>
2025-03-13 14:30:16 +0530

    leapyear finish

---
+++ problem4/file.java
@@ -0,0 +1,8 @@
+public class file{
+    public static void main(String[] args){
+        int i=2020;
+            if(i%4==0){
+                System.out.println("this is leap year:"+i);
+            }
+        }
+    }
