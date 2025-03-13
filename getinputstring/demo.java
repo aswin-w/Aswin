06ab4371bbb37df02ec9a70d6f4dacada7b7485f
aswin-w <aswinwiselin24@gmail.com>
2025-03-13 15:15:45 +0530

    getinputstring finish

---
+++ getinputstring/demo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class demo {
+   public static void main(String[] args) {
+    Scanner scan=new Scanner(System.in);
+    String a=scan.nextLine();
+    System.out.println(a);
+   }
+    
+}
