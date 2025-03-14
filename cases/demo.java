5be6a330c7fbf0491149cb71893737f36257df2a
aswin-w <aswinwiselin24@gmail.com>
2025-03-14 12:03:51 +0530

    cases finish

---
+++ cases/demo.java
@@ -0,0 +1,9 @@
+public class demo{
+
+    public static void main(String[] args) {
+       String a="hello world";
+       
+       System.out.println(a.toUpperCase());
+       System.out.println(a.toLowerCase());
+    }
+}
