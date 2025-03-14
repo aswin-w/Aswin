ea25dcbe760970673a6a7985df424ef3e5b6da7c
aswin-w <aswinwiselin24@gmail.com>
2025-03-14 09:41:19 +0530

    age finish

---
+++ getinputage/age.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class age{
+    public static void main(String[] args){
+        System.out.print("enter your age:");
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        System.out.println("your age is:"+a);
+    }
+}
