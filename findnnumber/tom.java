291a5307e1bae6fc90d1528582aafbf8acc90277
aswin-w <aswinwiselin24@gmail.com>
2025-03-14 10:19:20 +0530

    n numder finish

---
+++ findnnumber/tom.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class tom{
+    public static void main(String[] args){
+        int i =1;
+        Scanner scan = new Scanner (System.in);
+        System.out.print("enter the input:");
+        int j=scan.nextInt();
+        while(i<=j){
+            System.out.println(i);
+            i++;
+        }
+    }
+        }
