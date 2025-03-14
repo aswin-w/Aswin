7675e239145500c3b0d36844d0308460ef6e6d00
aswin-w <aswinwiselin24@gmail.com>
2025-03-14 12:08:48 +0530

    findindex finish

---
+++ findindex/tom.java
@@ -0,0 +1,9 @@
+public class tom{
+
+    public static void main(String[] args) {
+       String a="hello world";
+       
+       System.out.println(a.indexOf("w"));
+       System.out.println(a.indexOf(" "));
+    }
+}
