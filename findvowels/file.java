8724d30e4ee1b7d04be733be0a1c9c92bc3f0059
aswin-w <aswinwiselin24@gmail.com>
2025-03-14 11:03:40 +0530

    vowels finish

---
+++ findvowels/file.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class file{
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter the input:");
+        String a=scan.nextLine();
+        System.out.println(a);
+        String b[]={"a","e","i","o","u"};
+        
+        if(a.equals(b[0])){
+            System.out.println("this is vowels:"+a);
+        }
+        else if(a.equals(b[1])){
+            System.out.println("this is vowels:"+a);
+        }
+        else if(a.equals(b[2])){
+            System.out.println("this is vowels:"+a);
+        }
+        else if(a.equals(b[3])){
+            System.out.println("this is vowels:"+a);
+        }
+        else if(a.equals(b[4])){
+            System.out.println("this is vowels:"+a);
+        }
+        else{
+            System.out.println("this is not vowels.");
+        }
+    }
+}
+        
+        
+        
+        
+        
+     
+       
+    
+
