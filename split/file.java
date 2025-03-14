75fcd28c1d777ee5566faf7e370a9e6ed80891a1
aswin-w <aswinwiselin24@gmail.com>
2025-03-14 11:51:35 +0530

    split finish

---
+++ split/file.java
@@ -0,0 +1,14 @@
+public class file{
+
+    public static void main(String[] args) {
+       String a="hellow,world";
+       String b[]=a.split(",");
+       System.out.println(b[0]);
+       System.out.println(b[1]);
+
+
+
+
+
+    }
+}
