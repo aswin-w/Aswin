831b6334950a02c9a5b282a146d82418b15e7033
aswin-w <aswinwiselin24@gmail.com>
2025-03-14 10:03:18 +0530

    whileloop finish

---
+++ whileloop/file.java
@@ -0,0 +1,10 @@
+public class file
+{
+    public static void main(String[] args){
+        int i =92;
+        while(i<=98){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
